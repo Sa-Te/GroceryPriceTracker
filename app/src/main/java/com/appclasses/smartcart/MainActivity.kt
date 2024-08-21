3ff304f6aab8d2197d313db4aeca27b94faa162a
Sa-Te <saishark27@gmail.com>
2024-08-22 00:41:23 +0100

    Added folder structure and splash screen

--- app/src/main/java/com/appclasses/smartcart/MainActivity.kt
+++ app/src/main/java/com/appclasses/smartcart/MainActivity.kt
@@ -4,28 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity()
-{
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
-          setContentView(R.layout.activity_main)
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val intent = Intent(this@MainActivity, welcome::class.java)
+        // Delay for a few seconds and then start the welcome activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, welcome::class.java)
             startActivity(intent)
-            finish()
-        },2000)
-
-
+            finish() // Close this activity
+        }, 2000) // 2 seconds delay
     }
 }
